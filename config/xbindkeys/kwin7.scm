1b9954b609b6fd6e5f33df84a3e769f8770620b2
a <a@a>
2024-11-24 14:50:35 +0800

    freetile

--- config/xbindkeys/kwin7.scm
+++ config/xbindkeys/kwin7.scm
@@ -8,25 +8,25 @@
 (xbindkey '(mod4 "1")           "bash ~/bin/menu")
 
 
+(load ".config/xbindkeys/tile_full2.scm")
+(load ".config/xbindkeys/media2.scm")
+
 ;;print安装spectacle后就有默认键位了,不过注意全屏是shift print, 窗口是super print.
 ;;(xbindkey '("Print") "mkdir ~/show;cd ~/show;scrot")
 ;;(xbindkey '(alt "Print") "mkdir ~/show;cd ~/show;spectacle -b -a -o ~/show/1.png")
 ;;(xbindkey '(mod4 "w") "bash ~/bin/compiz/set_random_wallpaper.sh")
 
-(define (load-file path) 
-  (define home (getenv "HOME")) 
-  (define abspath (string-append home "/.config/xbindkeys/" path)) 
-  (load abspath))
+;;#(define (load-file path) 
+;;#  (define home (getenv "HOME")) 
+;;#  (define abspath (string-append home "/.config/xbindkeys/" path)) 
+;;#  (load abspath))
+;;#
+;;#(define (for-each func lst) 
+;;#  (let loop ((rest lst)) 
+;;#    (unless (null? rest) 
+;;#	    (func (car rest)) 
+;;#	    (loop (cdr rest)))))
 
-(define (for-each func lst) 
-  (let loop ((rest lst)) 
-    (unless (null? rest) 
-	    (func (car rest)) 
-	    (loop (cdr rest)))))
-
-(load-file "tile_full.scm")
-
-(load-file "media2.scm")
 ;;(load-file "media.scm")
 ;;(load-file "./desktop.scm")
 ;;(load-file "./other.scm")
