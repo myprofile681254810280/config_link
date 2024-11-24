8b16561c562c32eea6ababe38d48d8beedd6ecb7
a <a@a>
2024-11-24 15:45:22 +0800

    bash_filter.sh

--- local-share/bin/bash_filter.sh
+++ local-share/bin/bash_filter.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#见  ~/bin/filter/bash_filter.py
-#python ~/bin/filter/bash_filter.py $@ | bash
-#
-#经常性误用bash运行python.过滤下不要运行py后缀文件.
-#仅仅只过滤arg[0]参数
+#误用bash执行.py文件会产生难以预测的麻烦，所以执行前要检查
 if [[ $1 =~ \.py$ ]];
 then
     echo bash $@
