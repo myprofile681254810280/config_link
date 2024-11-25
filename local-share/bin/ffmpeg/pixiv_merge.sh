acd10cfe3914a1a508361dc40b5dbc6151e37ab9
a <a@a>
2024-11-25 14:38:36 +0800

    ffmpeg

--- local-share/bin/ffmpeg/pixiv_merge.sh
+++
@@ -1,8 +0,0 @@
-#
-#格式选择
-#apng ff57可用,但是太大
-#webm, 最小,但是质量差
-#webp 第二大, 并且dolphin不可预览
-#mp4 质量不差, dolphin可以预览,注意安装插件
-
-ffmpeg -framerate 15 -i "$1/%06d.jpg" "$1.mp4"
--- local-share/bin/ffmpeg/shift_sub.sh
+++
@@ -1,2 +0,0 @@
-#改变字幕延迟
-ffmpeg -i fin.ass -ss -00:00:01.0 fout.ass
